acd522d960466c25b85ce73df12a6195b5763339
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-20 23:32:45 -0400

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,16 @@
+function twoSum(array, targetSum){
+    let map = new Map();//this is to find if the array contains the complement.
+    for(let i = 0; i< array.length;i++){
+        complement = targetSum-array[i]; //find the complement
+        if(map.has(complement)){//success
+            let index1 =map.get(complement) ;
+            let index2 = i;
+            return [index1,index2];
+        }
+        //if no complement update complement
+        map.set(array[i], i);
+    }
+    return null;
+}
+
+module.exports = twoSum;
