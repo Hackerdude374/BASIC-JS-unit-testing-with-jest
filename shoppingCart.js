19bb5e19364c13c31ce98b19acb2c27630e6a63d
Hackerdude374 <136039952+Hackerdude374@users.noreply.github.com>
2023-06-20 23:12:09 -0400

    Update shoppingCart.js

--- shoppingCart.js
+++ shoppingCart.js
@@ -5,18 +5,25 @@ function addToCart(item, quantity){
     //case 1: item already in cart, update the quantity 
     if(item in this.cart)
     this.cart[item] +=quantity;
-    else
+    else//no item so quantity is the given
     this.cart[item] = quantity;
 }
 
 function removeFromCart(item, quantity){
+    //case 0: no items exist 
+    if(this.cart[item] == 0){
+        return null;
+    }
     //case 1: item quantity = quantity deleted  or less = delete the whole thing
     if(this.cart[item] <= quantity){
         delete this.cart[item];
+        return true;
     }
     else
     this.cart[item]-=quantity;
+    return true
 }
+
 function calculateTotal(){
 let sum = 0;
 for(let item in this.cart){
